b175cdcca4c5d6758293155451af5133a74f9fb2
egg4096 <72076491+egg4096@users.noreply.github.com>
2022-01-15 16:31:27 +0000

    good code

--- translate.py
+++ translate.py
@@ -1,83 +1,80 @@
-input_file = open("input.txt", mode="r", encoding="utf-8")
-output_file = open("output.html", mode="w", encoding="utf-8")
+with open('input.txt') as source:
+	lines = source.readlines()
 
 level = 0
-lines = input_file.readlines()
 tags = []
 
-output_file.write("<!DOCTYPE html>\n\n<html>\n")
-
 def add(text):
-    output_file.write(f"\t{text}")
+	destination.write(f'\t{text}')
 
-for line in lines:
-    line_stripped = line.strip()
+with open('output.html', mode='w', encoding='utf-8') as destination:
+	destination.write('<!DOCTYPE html>\n\n<html>\n')
 
-    if line_stripped.endswith("]"):
-        if "[" in line:
-            partition = line.partition("[")
-            closes = partition[2].count("]") - 1
+	for line in lines:
+	    line_stripped = line.strip()
 
-            try:
-                text = partition[0].strip() # h2 with properties
-                spaces = partition[0].partition(text[0])[0]
-                text_cut = text.partition(" ")[0] # h2
-                content = partition[2][:partition[2].index("]")]
-                add(f"{spaces}<{text}>{content}</{text_cut}>\n")
+	    if line_stripped.endswith(']'):
+	        if '[' in line:
+	            partition = line.partition('[')
+	            closes = partition[2].count(']') - 1
+
+	            try:
+	                text = partition[0].strip() # h2 with properties
+	                spaces = partition[0].partition(text[0])[0]
+	                text_cut = text.partition(' ')[0] # h2
+	                content = partition[2][:partition[2].index(']')]
+	                add(f'{spaces}<{text}>{content}</{text_cut}>\n')
 
 
-                for _ in range(closes):
-                    level -= 1
+	                for _ in range(closes):
+	                    level -= 1
 
-                    try:
-                        spaces = tags[level].partition(tags[level].strip()[0])[0]
-                        text_cut = tags[level].strip().partition(" ")[0]
-                        add(f"{spaces}</{text_cut}>\n")
-                        tags.pop(level)
-                    except Exception:
-                        pass
-            except Exception:
-                pass
-        else:
-            level -= 1
+	                    try:
+	                        spaces = tags[level].partition(tags[level].strip()[0])[0]
+	                        text_cut = tags[level].strip().partition(' ')[0]
+	                        add(f'{spaces}</{text_cut}>\n')
+	                        tags.pop(level)
+	                    except Exception:
+	                        pass
+	            except Exception:
+	                pass
+	        else:
+	            level -= 1
 
-            try:
-                spaces = tags[level].partition(tags[level].strip()[0])[0]
-                text_cut = tags[level].strip().partition(" ")[0]
-                add(f"{spaces}</{text_cut}>\n")
-                tags.pop(level)
-            except Exception:
-                pass
-    elif line_stripped.endswith("[") and line_stripped != "[":
-        bit = line.partition("[")[0]
-        bit_stripped = bit.strip()
+	            try:
+	                spaces = tags[level].partition(tags[level].strip()[0])[0]
+	                text_cut = tags[level].strip().partition(' ')[0]
+	                add(f'{spaces}</{text_cut}>\n')
+	                tags.pop(level)
+	            except Exception:
+	                pass
+	    elif line_stripped.endswith('[') and line_stripped != '[':
+	        bit = line.partition('[')[0]
+	        bit_stripped = bit.strip()
 
-        try:
-            spaces = bit.partition(bit_stripped[0])[0]
-        except Exception:
-            pass
+	        try:
+	            spaces = bit.partition(bit_stripped[0])[0]
+	        except Exception:
+	            pass
 
-        if bit_stripped[0] == "]":
-            bit = bit_stripped[1:]
-            add(f"{spaces}</{bit.strip()}>\n")
-            tags.pop(level - 1)
-        else:
-            level += 1
+	        if bit_stripped[0] == ']':
+	            bit = bit_stripped[1:]
+	            add(f'{spaces}</{bit.strip()}>\n')
+	            tags.pop(level - 1)
+	        else:
+	            level += 1
 
-        bit_stripped = bit.strip()
-        tags.append(f"{spaces}{bit_stripped}")
-        add(f"{spaces}<{bit_stripped}>\n")
-    elif lines[lines.index(line) + 1 if lines.index(line) + 1 < len(lines) else len(lines) - 1].strip() == "[":
-        level += 1
-        tags.append(f"{spaces}{line_stripped}")
-        add(f"<{line_stripped}>\n")
-    elif line_stripped != "[" and not line_stripped.startswith("#"):
-        add(line)
+	        bit_stripped = bit.strip()
+	        tags.append(f'{spaces}{bit_stripped}')
+	        add(f'{spaces}<{bit_stripped}>\n')
+	    elif lines[lines.index(line) + 1 if lines.index(line) + 1 < len(lines) else len(lines) - 1].strip() == '[':
+	        level += 1
+	        tags.append(f'{spaces}{line_stripped}')
+	        add(f'<{line_stripped}>\n')
+	    elif line_stripped != '[' and not line_stripped.startswith('#'):
+	        add(line)
 
-output_file.write("</html>\n");
-
-input_file.close()
-output_file.close()
+	destination.write('</html>\n');
 
 if tags:
-    print(f"Warning! Unclosed tags: {tags}.")
+    print(f'Warning! Unclosed tags: {tags}.')
